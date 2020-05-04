c560d6cbecae27b42182dd149e3c967fef25dce3
saijo-shota-biz <saijo.shota.biz@gmail.com>
2020-05-05 08:25:21 +0900

    add captor 4 create folder

--- server/routes/web.php
+++ server/routes/web.php
@@ -13,4 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
+// Task
+Route::get("/folders/{id}/tasks", "TaskController@index")->name("tasks.index");
+
+// Folder
+Route::get("/folders/create", "FolderController@showCreateForm")->name("folders.create");
+Route::post("/folders/create", "FolderController@create");
