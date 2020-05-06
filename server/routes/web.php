fab01073534320c3d003b5ec7fce3d9f606e634f
saijo-shota-biz <saijo.shota.biz@gmail.com>
2020-05-07 00:41:41 +0900

    add capter10 error handling

--- server/routes/web.php
+++ server/routes/web.php
@@ -19,16 +19,20 @@ Route::group(['middleware' => 'auth'], function () {
   // Home
   Route::get('/', 'HomeController@index')->name('home');
 
-  // Task
-  Route::get("/folders/{id}/tasks", "TaskController@index")->name("tasks.index");
-  Route::get("/folders/{id}/tasks/create", "TaskController@showCreateForm")->name("tasks.create");
-  Route::post("/folders/{id}/tasks/create", "TaskController@create");
-  Route::get("/folders/{id}/tasks/{task_id}/edit", "TaskController@showEditForm")->name("tasks.edit");
-  Route::post("/folders/{id}/tasks/{task_id}/edit", "TaskController@edit");
-
   // Folder
   Route::get("/folders/create", "FolderController@showCreateForm")->name("folders.create");
   Route::post("/folders/create", "FolderController@create");
+
+  // Task
+  Route::group(['middleware' => 'can:view,folder'], function () {
+    Route::get('/folders/{folder}/tasks', 'TaskController@index')->name('tasks.index');
+
+    Route::get("/folders/{folder}/tasks/create", "TaskController@showCreateForm")->name("tasks.create");
+    Route::post("/folders/{folder}/tasks/create", "TaskController@create");
+
+    Route::get("/folders/{folder}/tasks/{task}/edit", "TaskController@showEditForm")->name("tasks.edit");
+    Route::post("/folders/{folder}/tasks/{task}/edit", "TaskController@edit");
+  });
 });
 
 Auth::routes();
